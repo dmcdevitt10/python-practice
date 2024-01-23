1477f1a0aab00c7cc52a73bceb9664c62c651469
Daniel McDevitt <danielmcdevitt10@gmail.com>
2024-01-22 21:27:15 -0700

    added ASCII art for caesar cipher

--- practice.py
+++ practice.py
@@ -571,6 +571,10 @@
 
 
 #caesar cipher
+import cipher_art
+logo = cipher_art.logo
+print(logo)
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
