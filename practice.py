19f2516c295a5e348209321bc3a96348de85e53b
Daniel McDevitt <danielmcdevitt10@gmail.com>
2024-01-23 12:11:11 -0700

    created an algorithm that outputs number of days in a month based off the year (checks if year is a leap year in case month is february)

--- practice.py
+++ practice.py
@@ -654,35 +654,73 @@
 
 
 
-#auction
-import auction_art
-print(auction_art.logo)
-print("Welcome to the auction!\n\n")
+# #auction
+# import auction_art
+# print(auction_art.logo)
+# print("Welcome to the auction!\n\n")
 
 
-bids = {}
+# bids = {}
 
-more_bidders = True
+# more_bidders = True
 
-winner = ""
-winning_bid = 0
+# winner = ""
+# winning_bid = 0
 
-while more_bidders:
+# while more_bidders:
 
-    name = input("What is your name?: ")
-    bid = input("What is your bid?: ")
+#     name = input("What is your name?: ")
+#     bid = input("What is your bid?: ")
 
-    bids[name] = int(bid)
+#     bids[name] = int(bid)
 
-    other_bidders = input("Are there any other bidders?")
+#     other_bidders = input("Are there any other bidders?")
 
-    if other_bidders == "no":
-        more_bidders = False
+#     if other_bidders == "no":
+#         more_bidders = False
 
-for bidder in bids:
-    if bids[bidder] > winning_bid:
-        winner = bidder
-        winning_bid = bids[bidder]
+# for bidder in bids:
+#     if bids[bidder] > winning_bid:
+#         winner = bidder
+#         winning_bid = bids[bidder]
 
-print(bids)
-print(f"The winner is {winner} with a bid of ${winning_bid}.")
+# print(bids)
+# print(f"The winner is {winner} with a bid of ${winning_bid}.")
+
+
+
+
+# Functions with outputs (return)
+# def my_function():
+#     return "hello"
+
+
+# print(my_function())
+
+
+
+
+#days in a month based off year
+def is_leap(year):
+  if year % 4 == 0:
+    if year % 100 == 0:
+      if year % 400 == 0:
+        return True
+      else:
+        return False
+    else:
+      return True
+  else:
+    return False
+  
+def days_in_month(year, month):
+  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] 
+  if is_leap(year):
+    month_days[1] += 1
+  return month_days[month - 1]
+  
+  
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
+days = days_in_month(year, month)
+print(days)
