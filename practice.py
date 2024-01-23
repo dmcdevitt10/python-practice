e42be0fbfe4ed7fe6020ad79201b36dea72c3a83
Daniel McDevitt <danielmcdevitt10@gmail.com>
2024-01-23 15:50:10 -0700

    created a calculator algorithm that involves recursion

--- practice.py
+++ practice.py
@@ -701,26 +701,75 @@
 
 
 #days in a month based off year
-def is_leap(year):
-  if year % 4 == 0:
-    if year % 100 == 0:
-      if year % 400 == 0:
-        return True
-      else:
-        return False
-    else:
-      return True
-  else:
-    return False
+# def is_leap(year):
+#   if year % 4 == 0:
+#     if year % 100 == 0:
+#       if year % 400 == 0:
+#         return True
+#       else:
+#         return False
+#     else:
+#       return True
+#   else:
+#     return False
   
-def days_in_month(year, month):
-  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] 
-  if is_leap(year):
-    month_days[1] += 1
-  return month_days[month - 1]
+# def days_in_month(year, month):
+#   month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] 
+#   if is_leap(year):
+#     month_days[1] += 1
+#   return month_days[month - 1]
   
   
-year = int(input("Enter a year: "))
-month = int(input("Enter a month: "))
-days = days_in_month(year, month)
-print(days)
+# year = int(input("Enter a year: "))
+# month = int(input("Enter a month: "))
+# days = days_in_month(year, month)
+# print(days)
+
+
+
+
+# Calculator (with recursion)
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+
+def calculator():
+    number1 = float(input("What's the first number?: "))
+    for symbol in operations:
+        print(symbol)
+        
+    should_continue = True
+
+    while should_continue:
+
+        operation_symbol = input("Pick an operation: ")
+        number2 = float(input("What's the next number?: "))
+
+        function = operations[operation_symbol]
+        answer = function(number1, number2)
+
+        print(f"{number1} {operation_symbol} {number2} = {answer}")
+
+        if input(f"Type 'y' to continue calculation with {answer}: ") == "y":
+            number1 = answer
+        else:
+            should_continue = False
+            calculator()
+
+calculator()
