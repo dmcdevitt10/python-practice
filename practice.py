9ceefb5e0f0b234e168e4a7610d3001864b57251
Daniel McDevitt <danielmcdevitt10@gmail.com>
2024-01-31 16:00:15 -0700

    using the PrettyTable package to make a table

--- practice.py
+++ practice.py
@@ -929,12 +929,22 @@
 
 
 # Object Oriented Programming
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
 
-timmy = Turtle()
-timmy.shape("turtle")
-timmy.color("chartreuse2")
+# timmy = Turtle()
+# timmy.shape("turtle")
+# timmy.color("chartreuse2")
+# timmy.forward(100)
 
-my_screen = Screen()
+# my_screen = Screen()
 
-my_screen.exitonclick()
+# my_screen.exitonclick()
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+
+print(table)
